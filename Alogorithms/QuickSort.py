fcafda57a27218d76bb85229fa88a6b63c4e7bc3
ShawnArunjith <shawnarun09@gmail.com>
2024-08-18 13:02:13 +0530

    FEAT :  New Impl

--- Alogorithms/QuickSort.py
+++ Alogorithms/QuickSort.py
@@ -2,16 +2,35 @@ import random
 
 
 def QuickSort(array):
+
     if len(array) < 1:
         return array
 
     pivot = random.choice(array)
-    left = [i for i in array if i < pivot]
+
+    left=[i for i in array if i< pivot]
+    middle = [i for i in array if i== pivot]
     right = [i for i in array if i > pivot]
-    middle = [i for i in array if i == pivot]
-
-    return QuickSort(left) + middle + QuickSort(right)
 
 
-arr = [4, 2, 5, 8, 7, 9, 10, 877, 56]
+    # left =[]
+    #
+    # for i in array:
+    #     if i < pivot:
+    #         left.append(i)
+    #
+    # middle =[]
+    # for i in array:
+    #     if i == pivot:
+    #         middle.append(i)
+    #
+    # right = []
+    # for i in array:
+    #     if i > pivot:
+    #         right.append(i)
+
+    return QuickSort(left) + middle +QuickSort(right)
+
+
+arr = [4, 2, 5, 8, 7]
 print(QuickSort(arr))
